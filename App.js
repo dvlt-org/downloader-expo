706f7d270f832b69d6b88307da88302de4c3a6f9
dvltinv <dvltinv@gmail.com>
2024-12-24 17:57:20 +0500

    downloader-version2

---
+++ App.js
@@ -0,0 +1,123 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import DownloadingScreen from "./screens/Downloading.js";
+import UploadedScreen from "./screens/Uploaded.js";
+import Drawer from "./screens/Drawer.js";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useRef } from "react";
+import { Animated, TouchableWithoutFeedback, View } from "react-native";
+
+const bottomTab = createBottomTabNavigator();
+
+const CustomBottom = ({ children, onPress }) => {
+  const animationValue = useRef(new Animated.Value(0)).current;
+
+  const handlePressIn = () => {
+    Animated.timing(animationValue, {
+      toValue: -40,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+    onPress();
+  };
+
+  const handlePressOut = () => {
+    Animated.timing(animationValue, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+  return (
+    <TouchableWithoutFeedback onPressIn={handlePressIn} onPressOut={handlePressOut}>
+      <Animated.View
+        style={{
+          transform: [
+            {
+              translateY: animationValue,
+            },
+          ],
+        }}
+      >
+        {children}
+      </Animated.View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <bottomTab.Navigator
+        initialRouteName="Home"
+        screenOptions={({ route }) => {
+          return {
+            tabBarIcon: ({ focused, size, color }) => {
+              let iconName;
+              if (route.name === "Home") {
+                iconName = "home";
+              } else if (route.name === "Downloading") {
+                iconName = "download";
+              } else {
+                iconName = "upload";
+              }
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+            headerShown: false,
+            tabBarShowLabel: true,
+            tabBarActiveTintColor: "#6A92BB",
+            tabBarStyle: {
+              height: 60,
+              elevation: 0,
+              borderTopWidth: 0,
+              backgroundColor: "#f8f8f8", // Orqa fon rangi
+            },
+            tabBarItemStyle: {
+              width: 100, // Icon va labelni markazlashtirish
+              justifyContent: "center",
+              alignItems: "center",
+              flexDirection: "column",
+            },
+            tabBarLabelStyle: {
+              fontSize: 12, // Labelning o‘lchami
+              fontWeight: "bold", // Label qalinligi
+              marginTop: 5, // Ikon va label orasidagi masofa
+              color: "#808080", // Labelning rangini belgilash
+            },
+            tabBarIconStyle: {
+              marginBottom: 0, // Ikon va label o'rtasidagi masofani boshqarish
+            },
+          };
+        }}
+      >
+        <bottomTab.Screen
+          name="Home"
+          component={Drawer}
+          options={() => {
+            return {
+              tabBarButton: (props) => <CustomBottom {...props} />,
+            };
+          }}
+        />
+        <bottomTab.Screen
+          name="Downloading"
+          component={DownloadingScreen}
+          options={() => {
+            return {
+              tabBarButton: (props) => <CustomBottom {...props} />,
+            };
+          }}
+        />
+        <bottomTab.Screen
+          name="Uploaded"
+          component={UploadedScreen}
+          options={() => {
+            return {
+              tabBarButton: (props) => <CustomBottom {...props} />,
+            };
+          }}
+        />
+      </bottomTab.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ components/DownloadVideo.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { Video } from "expo-av"; // expo-av paketidan Video komponentini import qilish
+
+const DownloadedVideo = ({ route, navigation }) => {
+  const { uri } = route.params; 
+
+  const [status, setStatus] = useState({});
+  
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Downloaded Video</Text>
+      
+      {/* Video oynatish */}
+      <Video
+        source={{ uri: uri }} // Video faylning manzili
+        style={styles.video}
+        useNativeControls
+        resizeMode="contain"
+        onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      />
+
+      {/* Ekranga qaytish uchun tugma */}
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    padding: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  video: {
+    width: "100%",
+    height: 300,
+    backgroundColor: "#000",
+  },
+});
+
+export default DownloadedVideo;
